83f9d0116ea45896ffce6bd5580d4fa0cf986c91
Sondre Bye Humberset <sondre@apt.no>
2021-05-28 00:29:35 +0200

    Add base font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import type { AppProps } from "next/app";
 
 import "normalize.css";
+import "@styles/base/base.scss";
 
 function MyApp({ Component, pageProps }: AppProps)
 {
