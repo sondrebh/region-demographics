a659333beb8ecdb2c924c0169f42bfd7287ee727
Sondre Bye Humberset <sondre@apt.no>
2021-05-28 10:00:54 +0200

    Add RegionSelector

---
+++ src/components/RegionSelector/RegionSelector.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Country from "CountryTypes";
+import "./RegionSelector.module.scss";
+
+interface RegionSelectorProps
+{
+    allCountriesData: Array<Country>
+}
+
+function RegionSelector(props: RegionDemographicsProps)
+{
+    return (
+        <>
+            <p>RegionSelector</p>
+        </>
+    );
+}
+
+export default RegionSelector;
