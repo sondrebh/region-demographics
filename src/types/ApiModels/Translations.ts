8503f39dda6699238d0c6489bc8ce369916433d9
Sondre Bye Humberset <sondre@apt.no>
2021-05-29 02:16:10 +0200

    Add ApiModels types

---
+++ src/types/ApiModels/Translations.ts
@@ -0,0 +1,16 @@
+declare module "ApiModels"
+{
+	export interface Translations
+	{
+		de: string;
+		es: string;
+		fr: string;
+		ja: string;
+		it: string;
+		br: string;
+		pt: string;
+		nl: string;
+		hr: string;
+		fa: string;
+	}
+}
